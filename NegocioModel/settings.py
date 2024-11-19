21337079d1b75e3d25ef863813939b39512c9fec
Eduardo Muñoz <cgustavlaptop@MacBook-Pro-de-Eduardo.local>
2024-11-19 02:38:22 -0300

    :bug: Adding Dotenv load on settings.py

--- NegocioModel/settings.py
+++ NegocioModel/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+
+# Cargar variables de entorno
+load_dotenv(os.path.join(Path(__file__).resolve().parent.parent, '.env'))
+
+print("Current DB_HOST: ", os.getenv("DB_HOST", "no value set"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
